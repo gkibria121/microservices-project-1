0438658cf021a461593b57981ce2505830b1d5b4
gkibria121 <gkibria121@gmail.com>
2025-03-06 13:29:52 +0600

    add script to build and push

--- bin/build.sh
+++ bin/build.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-
+# Check if the directory exists
 if [ -d "$1" ]; then
+    echo "Building $1"
+    cd "$1" && docker build -t gkibria121/"$1" -f ../dockerfiles/Dockerfile .
 
-echo "building $1"
-    cd $1 && docker build -t gkibria121/$1 -f ../dockerfiles/Dockerfile  .
-
+    # Check if --push flag is provided
+    if [ "$2" = "--push" ]; then
+        echo "Pushing gkibria121/$1 to Docker Hub..."
+        docker push gkibria121/"$1"
+    fi
 else
     echo "Error: Directory $1 does not exist."
-
 fi
